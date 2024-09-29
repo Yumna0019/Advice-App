c7452c3db3ce08a310c749eeb0acef2e3d5be6bd
Yumna <yumnamubeen0019@gmail.com>
2024-09-29 16:35:55 +0500

    App completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import './index.css';
-import axios from 'axios';
+import "./index.css";
+import axios from "axios";
+import { useState , useEffect } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState('');
+
+  const fetchAdvice = () => {
+    axios.get("https://api.adviceslip.com/advice")
+      .then((response) => setAdvice(response.data.slip.advice))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  },[]);
+
   return (
-
-
-
-
-    <div className="App">
-      <h2>advice</h2>
-    </div>
+    <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button" onClick={fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
   );
 }
 
